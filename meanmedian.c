19afcf21a915c4801195373d22f73c28a8aad338
vinodkumar1111 <104495871+vinodkumar1111@users.noreply.github.com>
2022-05-04 15:42:12 +0530

    Add files via upload

--- meanmedian.c
+++ meanmedian.c
@@ -1,26 +1,30 @@
-
-
 #include <stdio.h>
-
+float mean(int *array, int len)
+{
+    float sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum = sum + array[i];
+    }
+    float mean = sum / len;
+    return mean;
+}
+float median(int *array,int len)
+{
+    float median=0;
+    if(len%2==0)
+    return(array[len/2] + array[(len/2)-1])/2;
+      else
+      return array[len/2];
+return median;
+}
 int main()
 {
-    int array[]={1,2,3,4,5,6,7};
-         int  sum=0,mean=0;
-         float median=0;
-
-    int len=sizeof(array)/sizeof(array[0]);
-    printf("length of array is: %d\n",len);
-    printf("sum of given elements is:");
-    for(int i=0;i<len;i++){
-     sum = sum+array[i];
-    }
-     printf("%d\n",sum);
-    mean=sum/len;
-    printf("mean of given elements is:%d\n",mean);
-    if(len%2==0)
-      median=(array[len/2] + array[(len/2)-1])/2;
-      else
-      median = array[len/2];
-      printf("median of given elements is:%f",median);
+    int array[] = {1, 2, 3, 4, 5, 6, 7,8};
+    int len = sizeof(array) / sizeof(int);
+    float array_mean = mean(array, len);
+    printf("mean is %f\n", array_mean);
+    float array_median=median(array,len);
+    printf("median is %f",array_median);
     return 0;
 }
